ca79a52276f3c15d7349d63e09b409f52193b11c
Mohamed Farhan <farhanfaleel9@gmail.com>
2026-07-23 04:00:37 +0530

    Delete db.php

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$connect = mysqli_connect("localhost", "root", "", "car_booking");
-
-if(!$connect){
-    die("Connection Failed: " . mysqli_connect_error());
-}
-// echo 'Connection Success';
-?>
